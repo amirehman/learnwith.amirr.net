0383a735b6835501f3b3e9f4f692d3e014246f64
Aamir <aamirnorth@gmail.com>
2023-02-19 13:18:08 +0400

    First Commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen w-full flex items-center justify-center">
+    <h1 class="text-2xl tracking-widest lowercase">
+      <span class="text-red-600">learnwith.</span>amirr.net
+    </h1>
+  </div>
+</template>
